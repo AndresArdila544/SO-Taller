398d4918725f1876d06170f8554881c91b6c13c1
ncontrerasn <36159522+ncontrerasn@users.noreply.github.com>
2020-05-20 21:42:45 -0500

    Add files via upload

--- memoriaCompartida.c
+++ memoriaCompartida.c
@@ -8,13 +8,11 @@
 int main()
 {
     pid_t pid;
-    key_t key = 8978;
+    key_t key = 9849;
     int shmId;
-    long long a, b, c, *ap, *vector;
+    long long a, b, c, *ap;
     int status, cpid, r, i, tope = 12500000, contador = 0;
-    vector = (long long *)malloc(tope * sizeof(long long));
-    for(i = 0; i < tope; i++)
-        vector[i] = rand();
+    
     //crear espacio de memoria compartida
     shmId = shmget(key, tope * sizeof(long long), 0666 | IPC_CREAT);
     if(shmId < 0){
@@ -24,25 +22,16 @@ int main()
     //asociar espacio de memoria creada a este proceso
     ap = (long long *)shmat(shmId, 0, 0);
     pid = fork();
-    if (pid == -1){  //handle errors
-        perror("error fork"); 
-        exit(EXIT_FAILURE); 
+
+    if(pid == 0){ //hijo
+        for(i = 0; i < tope; i++)
+            *(ap + i)= rand();
+        
     }
-    if(pid == 0) //hijo
-        //for(i = 0; i < tope; i++)
-            *ap = *vector;
     else{ //padre
-        if (wait(&status) == pid){ //esperar a hijo
-            //for(i = 0; i < tope; i++)
-                //printf("\ndato %lli", *(ap + i));
-              //  contador++;
-            //}   
-        }
     }
+    //printf("valor fM: %lli", *(ap + tope - 1));
     r = shmdt(ap);  //desasociar espacio de memoria compartida
-    if(r < 0){
-      perror("error shmdt ");
-      exit(-1);
-    }
+    return 0;
     
 }
